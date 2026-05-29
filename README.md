b783a51a88b4a2571d81f99b089ecf537d11f72c
Dongyu Zhang <dongyuz2019@gmail.com>
2026-05-30 09:51:42 +1200

    2026-05-23 send message

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # live-chat
+
+## Issues
+1. input suggestion to be off.
+2. after sending, the input is not cleared.
+3. there is a delay between and after sending messages
+4. after sending too much you can't send anymore
--- index.js
+++ index.js
@@ -24,12 +24,21 @@ app.post('/join', (req, res) => {
         nickname: 'System',
         message: `Welcome ${req.body.nickname}`,
         datetime: new Date()
-    })
-    res.render('chat');
+    });
+    res.render('chat', { nickname: req.body.nickname });
 });
 
 app.get('/poll', (req, res) => {
     res.status(200).json(chat);
+});
+
+app.post('/send', (req, res) => {
+    chat.history.push({
+        nickname: req.body.nickname,
+        message: req.body.messageContent,
+        datetime: new Date()
+    });
+    res.status(200);
 });
 
 app.listen(3000, () => {
